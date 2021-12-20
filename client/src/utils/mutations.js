dd92d569b51e9370b96ebf65f7250684fd42d897
jyoung0613 <jwyoung0613@gmail.com>
2021-12-20 11:46:25 -0700

    fixed issues with graphql had to add query and fixed signup and login

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -28,6 +28,13 @@ export const ADD_USER = gql`
       username: $username
       email: $email
       password: $password
-    )
+    ) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
   }
 `;
